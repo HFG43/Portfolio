b6dbb83d970df38796b346779e63b1b20e0c11b4
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-05-15 12:50:17 -0600

    Add hamburguer close function

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const hamburguer = document.getElementById('#hamburguer');
-const mobilemenu = document.querySelector('#mobile-menu-hidden');
 
-hamburguer.addEventListener(('click'),function() {
-  mobilemenu.classList.remove('hidden');
+const menuContainer = document.querySelector('#mobile-menu-hidden');
+const hamburguer = document.querySelector('#hamburguer');
+
+function displayMobileMenu() {
+  menuContainer.classList.remove('hidden');
 }
-
-// hamburguer.addEventListener(('click'), function(showMenu) {mobilemenu.classList.remove('hidden')});
+hamburguer.addEventListener('click', displayMobileMenu);
