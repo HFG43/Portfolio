2335646d77dc19f221c2cd7240600d410fb0616f
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-05-15 11:36:15 -0600

    Review Script

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const hamburguer = document.querySelector('.hamburguer-menu');
+const hamburguer = document.getElementById('#hamburguer');
 const mobilemenu = document.querySelector('#mobile-menu-hidden');
 
-hamburguer.addEventListener(('click'), function(showMenu) {mobilemenu.classList.remove('hidden')});
+hamburguer.addEventListener(('click'),function() {
+  mobilemenu.classList.remove('hidden');
+}
+
+// hamburguer.addEventListener(('click'), function(showMenu) {mobilemenu.classList.remove('hidden')});
