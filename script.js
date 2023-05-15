84f8587c9b17cac91ac4073b52caa460f0f58bb6
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-05-15 13:22:24 -0600

    Finish events functions
    Finished script
    Correct Awesome Icon cross selection

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 
 const menuContainer = document.querySelector('#mobile-menu-hidden');
 const hamburguer = document.querySelector('#hamburguer');
+const closeIcon = document.getElementById('closing-Icon');
+const menuListPortfolio = document.querySelector('.menu-mobile-portfolio');
+const menuListContact = document.querySelector('.menu-mobile-contact');
+const menuListAbout = document.querySelector('.menu-mobile-about');
 
 function displayMobileMenu() {
   menuContainer.classList.remove('hidden');
 }
 hamburguer.addEventListener('click', displayMobileMenu);
+
+
+function closeMobileMenu() {
+  menuContainer.classList.add('hidden');
+}
+menuListPortfolio.addEventListener('click', closeMobileMenu);
+menuListContact.addEventListener('click', closeMobileMenu);
+menuListAbout.addEventListener('click', closeMobileMenu);
+closeIcon.addEventListener('click', closeMobileMenu);
+
